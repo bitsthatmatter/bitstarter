deb5db59aa65497ab86c43e2f3aeaa722dacaed0
Kees Overboom <Kees.Overboom@gmail.com>
2013-07-04 22:13:10 +0200

    web.js reads index.html

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer;
+  fs.readFile('index.html', function (err, data) {
+     if (err) throw err;
+     buffer = new Buffer(data, "utf-8");
+  });
+  response.send(buffer.toString("utf-8");
 });
 
 var port = process.env.PORT || 5000;
