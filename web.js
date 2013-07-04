f47afa9c459c5d7e04425fdd1e73f63911a48bee
Kees Overboom <Kees.Overboom@gmail.com>
2013-07-04 22:27:25 +0200

    corrected errors in web.js

--- web.js
+++ web.js
@@ -4,12 +4,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer;
   fs.readFile('./index.html', function (err, data) {
      if (err) 
 	response.send('Could not find or open file index.html for reading\n');
      else {
-	buffer = new Buffer(data, 'utf-8');
+	var buffer = new Buffer(data, 'utf-8');
 	response.send(buffer.toString('utf-8');
      }
   });
