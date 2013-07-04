f9e15671624f3d31af59edba5b9525716b99bcd7
Kees Overboom <Kees.Overboom@gmail.com>
2013-07-04 22:24:07 +0200

    corrected errors in web.js

--- web.js
+++ web.js
@@ -1,14 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer;
-  fs.readFile('index.html', function (err, data) {
-     if (err) throw err;
-     buffer = new Buffer(data, "utf-8");
+  fs.readFile('./index.html', function (err, data) {
+     if (err) 
+	response.send('Could not find or open file index.html for reading\n');
+     else {
+	buffer = new Buffer(data, 'utf-8');
+	response.send(buffer.toString('utf-8');
+     }
   });
-  response.send(buffer.toString("utf-8");
 });
 
 var port = process.env.PORT || 5000;
